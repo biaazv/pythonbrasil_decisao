89d264c7cbf79f111b2dfa43abc90f73b18aab10
Beatriz <beatrizazv@gmail.com>
2024-11-26 21:09:14 -0300

    Adiciona atividade 13

---
+++ 13dia_semana.py
@@ -0,0 +1,20 @@
+#!/usr/local/bin/python3.11
+dia_semana = int(input("Digite um dia da semana: "))
+
+match dia_semana:
+    case 1:
+        print("Domingo")
+    case 2:
+        print("Segunda")
+    case 3:
+        print("Terça")
+    case 4:
+        print("Quarta")
+    case 5:
+        print("Quinta")
+    case 6:
+        print("Sexta")
+    case 7:
+        print("Sábado")
+    case _:
+        print("Valor inválido")
